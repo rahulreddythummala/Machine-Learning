5863cb4adb19bebd0cdcbf33ed346cb46109d0e9
Rahul Reddy Thummala <rthummala8@gmail.com>
2018-03-08 13:37:10 -0600

    Update R_data_preprocessing.py

--- Data Preprocessing/R_data_preprocessing.py
+++ Data Preprocessing/R_data_preprocessing.py
@@ -29,7 +29,7 @@ labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y) 
 
 #Splitting dataset into Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
  #feature scaling 
