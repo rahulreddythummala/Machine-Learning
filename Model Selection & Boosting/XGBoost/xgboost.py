9d11595e28cceaabb65daa8b91bb7eb99be68e96
Rahul Reddy Thummala <rthummala8@gmail.com>
2018-03-08 16:53:00 -0600

    Update xgboost.py

--- Model Selection & Boosting/XGBoost/xgboost.py
+++ Model Selection & Boosting/XGBoost/xgboost.py
@@ -1,8 +1,6 @@
 # XGBoost
 
-# Install xgboost following the instructions on this link: http://xgboost.readthedocs.io/en/latest/build.html#
-
-# Importing the libraries
+#Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
